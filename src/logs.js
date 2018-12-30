587413e26b91ca25c8dbb0480363b074f477c14e
Matias Surdi <matias@surdi.net>
2018-12-30 16:41:54 +0100

    Log to stdout too

--- src/logs.js
+++ src/logs.js
@@ -3,7 +3,10 @@ let logs = [];
 export default {
   log(message) {
     const date = new Date();
-    logs.push(`${date} ${message}`);
+    const formattedMessage = `${date} ${message}`;
+    logs.push(formattedMessage);
+    // eslint-disable-next-line no-console
+    console.log(formattedMessage);
   },
   clear() {
     logs = [];
