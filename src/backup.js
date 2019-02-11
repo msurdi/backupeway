b51d16b2c83e370fd1cba43f7e752c57302a0a4d
Matias Surdi <matias@surdi.net>
2019-02-11 21:37:07 +0100

    Fix interval configuration usage

--- src/backup.js
+++ src/backup.js
@@ -6,7 +6,7 @@ import config from './config';
 const MS_IN_A_DAY = 1000 * 60 * 60 * 24;
 
 const getOldestBackupToKeepDate = () => new Date(Date.now() - config.retentionDays * MS_IN_A_DAY);
-const backupInterval = config.retentionDays * MS_IN_A_DAY;
+const backupInterval = config.intervalDays * MS_IN_A_DAY;
 const logsInterval = config.logsDays * MS_IN_A_DAY;
 
 const client = axios.create({
